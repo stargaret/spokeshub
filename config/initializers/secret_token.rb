7ed6e390294a82caa7f99bfe72765a0174fb7fcf
Margaret Le <margaretminhle@gmail.com>
2011-04-09 14:55:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spokeshub::Application.config.secret_token = 'e9e2ce863c7ac1b5790e3c6efb1ae90c3983b003a8f3676f85ac5e741bc4623ed182f315d8d029a2ef6e5a9cf397f5de4c8b573da879c8dda662d0e6e446777b'
